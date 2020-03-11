b194ded8edd5d5863d6fc4a0f56dd1ba2eed0a8e
Milene Taborda <milenetaborda1@outlook.com>
2020-03-11 16:14:00 -0300

    Função criada para adicionar Imagem ao post

--- semana4/classes/blog/script.js
+++ semana4/classes/blog/script.js
@@ -1,8 +1,9 @@
-class postBlog {
-    constructor(title, autor, content) {
+class Post {
+    constructor(title, autor, content, image) {
         this.title = title
         this.autor = autor
         this.content = content
+        this.image = image
     }
 }
 
@@ -12,21 +13,45 @@ function addPostBlog() {
     const titleBlog = document.getElementById("input-title").value;
     const autorBlog = document.getElementById("input-autor").value;
     const contentBlog = document.getElementById("input-content").value;
-    
-    const newPost = new postBlog(titleBlog, autorBlog, contentBlog);
-
+        
+    const newPost = new Post(titleBlog, autorBlog, contentBlog);
     arrPost.push(newPost);
    
    const containerPost = document.getElementById("new-post");
    
    for(let post of arrPost){
     containerPost.innerHTML += "<div id='new-post-published'>" 
-                            + "<h2> Título:" + post.title + "</h2>"
-                            + "<h4> Autor: " + post.autor + "</h4>"
+                            + "<h2>" + post.title + "</h2>"
+                            + "<h4>" + post.autor + "</h4>"
                             + "<p>" + post.content + "</p>" 
                             + "</div>"
     } 
+
+    const imageBlog = document.getElementById("input-image");
+    if (
+        imageBlog.value.includes(".jpeg") ||
+        imageBlog.value.includes(".png") ||
+        imageBlog.value.includes(".jpg") ||
+        imageBlog.value.includes("data")
+    ) {
+        const image = document.getElementById("new-post-published");
+         image.innerHTML += "<img id='imgPost' src='" + imageBlog.value + "'/>";
+    }
 }
+
+function onKeyDownInput(e) {
+    if (e.key === "Enter") {
+      if (
+        e.target.value.includes(".jpeg") ||
+        e.target.value.includes(".png") ||
+        e.target.value.includes(".jpg") ||
+        e.target.value.includes("data")
+      ) {
+        const image = document.getElementById("new-post-published");
+        image.innerHTML += "<img id='imgPost' src='" + e.target.value + "'>";
+      } 
+    }
+  }
 
 function onKeyDown(e) {
     if (e.key === "Enter") {
