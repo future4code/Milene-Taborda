7c0e9a83f85d31ff9413f03be133af06d08e76dc
Milene Taborda <milenetaborda1@outlook.com>
2020-03-11 14:44:47 -0300

    Criação estrutura html

--- semana4/classes/blog/script.js
+++ semana4/classes/blog/script.js
@@ -1,2 +1,35 @@
+class postBlog {
+    constructor(title, autor, content) {
+        this.title = title
+        this.autor = autor
+        this.content = content
+    }
+}
 
+const arrPost = [];
 
+function addPostBlog() {
+    const titleBlog = document.getElementById("input-title").value;
+    const autorBlog = document.getElementById("input-autor").value;
+    const contentBlog = document.getElementById("input-content").value;
+    
+    const newPost = new postBlog(titleBlog, autorBlog, contentBlog);
+
+    arrPost.push(newPost);
+   
+   const containerPost = document.getElementById("new-post");
+   
+   for(let post of arrPost){
+    containerPost.innerHTML += "<div id='new-post-published'>" 
+                            + "<h2> Título:" + post.title + "</h2>"
+                            + "<h4> Autor: " + post.autor + "</h4>"
+                            + "<p>" + post.content + "</p>" 
+                            + "</div>"
+    } 
+}
+
+function onKeyDown(e) {
+    if (e.key === "Enter") {
+        addPostBlog()
+    }
+}
