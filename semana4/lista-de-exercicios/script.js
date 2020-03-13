7ad56fa5660e3766c4a353ae7444efbe3d8dbe80
Milene Taborda <milenetaborda1@outlook.com>
2020-03-13 11:19:14 -0300

    Exercício 1 - Atividade 2 / completo

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -2,3 +2,5 @@
 
 //Exercício 1 - Função que realiza a conversão do valor inserido no console de acordo com o valor da cotação em dólares passada no console. Resultado: 21,46
 
+//Exercício 2 - Função para realização de investimento, abaixo há uma verificação a partir da entrada de "valor/tipo" de investimento realizado, cada case há um tipo de rendimento diferente. Entretanto, se não houver nenhuma entrada de "valor/tipo", aparece um alerta na tela. Resultado: 1º console => 165; 2º console => alerta: Tipo de investimento informado incorreto!
+
