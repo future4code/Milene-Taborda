8c6639804e465cf551ff4e15c607dc2a8f8cea39
Milene Taborda <milenetaborda1@outlook.com>
2020-03-13 10:45:10 -0300

    Exercício 1 - Atividade 1 / completo

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,4 @@
+/* Exercício de interpretação de código*/
+
+//Exercício 1 - Função que realiza a conversão do valor inserido no console de acordo com o valor da cotação em dólares passada no console. Resultado: 21,46
+
