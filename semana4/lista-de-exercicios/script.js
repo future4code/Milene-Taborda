9cd3d82bd86cd96ed04be3603d2416d429e405af
Milene Taborda <milenetaborda1@outlook.com>
2020-03-13 16:31:20 -0300

    Exercício 5 - Atividade 3 / completo

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -185,3 +185,24 @@ const arrayParImpar = array.map(num => {
 
 console.log(arrayMultDois)
 */
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const pessoasComPermissao = pessoas.filter(pessoa => {
+    if (pessoa.altura >= 1.5 &&  pessoa.idade > 14 && pessoa.idade < 60)
+      return true;
+  });
+
+  const pessoasSemPermissao = pessoas.filter(pessoa => {
+    if (pessoa.altura < 1.5 ||  pessoa.idade < 14 || pessoa.idade >= 60)
+      return true;
+  });
+
+  console.log(pessoasSemPermissao)
