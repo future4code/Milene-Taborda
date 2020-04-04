3957a4bad390f970b9d1b50c4f28d041fc8d8161
Milene Taborda <milenetaborda1@outlook.com>
2020-04-04 13:12:00 -0300

    Adicionada algumas estilizações a mais

--- semana7/spotif4-projeto/src/App.js
+++ semana7/spotif4-projeto/src/App.js
@@ -59,7 +59,7 @@ export default class App extends Component {
 
       <ConatinerNavbar>
 
-      <Span><i class="fas fa-user-astronaut"> </i> <H1> SPOTIF4 - HAMILTON</H1></Span>
+      <Span><i class="fas fa-user-astronaut"> <H1> SPOTIF4</H1> </i></Span> 
 
         <ContainerNavBar>
           <ItemNavBar ><i class="fas fa-home"></i> Inicio</ItemNavBar >
@@ -110,8 +110,9 @@ const ConatinerNavbar = styled.div`
  
 `
 const Span = styled.div`
+  font-size: 20px;
   padding: 25px 10px;
 `
 const H1 = styled.h1 `
-  font-size: 10px;
+  
 `
