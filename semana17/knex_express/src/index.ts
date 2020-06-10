846db3114c00751e2d9e028f5de51b1b98af6ba0
Milene Taborda <milenetaborda1@outlook.com>
2020-06-10 15:32:30 -0400

    Exercicio incompleto

---
+++ semana17/knex_express/src/index.ts
@@ -0,0 +1,52 @@
+import * as knex from "knex";
+import * as dotenv from "dotenv";
+import * as express from 'express';
+import { AddressInfo } from "net";
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+const connection = knex({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+  },
+});
+
+const getActorById = async (id: string): Promise<any> => {
+  const result = await connection.raw(`
+      SELECT * FROM Actor WHERE id = '${id}'
+  `);
+  return result[0][0];
+};
+
+const searchActor = async (name: string): Promise<any>  =>{
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE name = '${name}'
+  `);
+  return console.log( result);
+};
+
+const countActors = async (gender: string): Promise<any> => {
+  const result = await connection.select().count("*").from("Actor").where({sex: gender});
+  console.log(result)
+}
+
+//searchActor("Tony Ramos");
+countActors("male");
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
+
